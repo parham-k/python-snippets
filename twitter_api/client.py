ba901d62da021993a55f0d5cce87f03465ad3368
Parham Kazemi <pkazemi3@gmail.com>
2018-09-21 23:00:33 +0430

    Updated twitter_api

--- twitter_api/client.py
+++ twitter_api/client.py
@@ -9,3 +9,6 @@ class TwitterClient:
         auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
         self.api = tweepy.API(auth, proxy='http://127.0.0.1:8123')
         self.user = self.api.get_user(username)
+
+    def get_user_tweet_texts(self, count=100):
+        return [tweet.text for tweet in self.api.user_timeline(count=count)]
--- twitter_api/sample.py
+++ twitter_api/sample.py
@@ -2,4 +2,4 @@ from twitter_api.client import TwitterClient
 
 if __name__ == '__main__':
     client = TwitterClient('random_redcoat')
-    print(client.api.home_timeline())
+    print('\n\n'.join(client.get_user_tweet_texts()))
